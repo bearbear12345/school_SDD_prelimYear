d8553b83dde7dc88d5379d28bd9e2492f198e08a
WONG <andrew.j.wong@DETNSW>
2016-05-26 10:41:53 +1000

    woa!! this one was really HARD!

--- Programming Work/Python/Hello World.py
+++ Programming Work/Python/Hello World.py
@@ -1,9 +1,8 @@
+#!/usr/bin/env python
+
 print("Hello World!\n")
 
-name = raw_input("What is your name? ")
+name = input("What is your name? ")
 while not name:
-    name = raw_input("What is your name? ")
+    name = input("What is your name? ")
 print("Hello, %s!" % name)
-
-import time
-time.sleep(2)
