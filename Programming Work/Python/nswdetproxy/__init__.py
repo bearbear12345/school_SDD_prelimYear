4ce30415614be0fac7042d24cc5d415b680f4cb7
Ando <andrew.j.wong@education.nsw.gov.au>
2016-08-20 20:32:51 +1000

    deprecation...?
    Might revert back

--- Programming Work/Python/nswdetproxy/__init__.py
+++ Programming Work/Python/nswdetproxy/__init__.py
@@ -1,2 +1,2 @@
-import urllib.request as r
+import urllib2 as r
 r.install_opener(r.build_opener(r.ProxyHandler({'http': r'http://username:password@proxy.det.nsw.edu.au:8080'}), r.HTTPBasicAuthHandler(), r.HTTPHandler))
